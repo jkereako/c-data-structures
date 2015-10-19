3431c38d6037a556d160d847902f95f82cbb95e1
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-10-19 12:42:25 -0400

    Split logic into multiple functions

--- src/linked-list.c
+++ src/linked-list.c
@@ -8,13 +8,72 @@ typedef struct item {
 } Item;
 
 Item * init_list(Item *);
+Item * append_item(Item *, Item *);
+Item * prepend_item(Item *, Item *);
+Item * allocate_item(void);
+Item * insert_item(Item *, Item *, int);
 Item * find_item(Item *, int);
 
-
 Item * init_list(Item *list) {
-  //assert(list);
-  list->value = 0;
+  assert(list != NULL);
+
   list->next_item = NULL;
+
+  return list;
+}
+
+
+Item * allocate_item(void) {
+  Item *item = (Item *)malloc(sizeof(Item));
+
+  return item;
+}
+
+Item * prepend_item(Item *list, Item *item) {
+  assert(list != NULL);
+  assert(item != NULL);
+
+  list->next_item = item;
+
+  return list;
+}
+
+Item * append_item(Item *list, Item *item) {
+  assert(list != NULL);
+  assert(item != NULL);
+  
+  list->next_item = item;
+  item->next_item = NULL;
+
+  return list;
+}
+
+Item * insert_item(Item *list, Item *item, int position) {
+  assert(list != NULL);
+  assert(item != NULL);
+  assert(position >= 0);
+  
+  if (position == 0) {
+   return prepend_item(list, item);
+  }
+  
+  Item *current_item = list;
+  // In order to insert a new item, we must find the item 1 position before the
+  // desired insert position.
+  int i = position -  1;
+
+  while(--i && current_item != NULL) {
+    current_item = current_item->next_item;
+  }
+ 
+  if (current_item == NULL) {
+    return append_item(list, item);
+  }
+
+  // Insert the new item
+  Item *temp = current_item->next_item;
+  current_item->next_item = item;
+  item->next_item = temp;
 
   return list;
 }
@@ -45,24 +104,22 @@ int main(int argc, char **argv) {
   Item *list, *current_item;
   
   // Initialize the list
-  list = current_item = (Item *)malloc(sizeof(Item));
-
+  current_item = list = allocate_item();
+   
+  //printf("\n  Got here. \n");
   init_list(list);
 
-  current_item->next_item = NULL;
-  
   // Create list items
-  for(i = 1; i < limit; i ++) {
+  for(i = 0; i < limit; i ++) {
     // When creating a new item, set the new item's address to the struct
     // memeber `next_item` and then set `next_item` to `current_item` to advance
     // the list.
-    current_item->next_item = (Item *)malloc(sizeof(Item));
-    current_item = current_item->next_item;
+    Item *item = allocate_item();
+    append_item(current_item, item);
     current_item->value = i;
+    current_item = current_item->next_item;
   }
   
-  // Set the last item to NULL to indicate that there are no more items.
-  current_item->next_item = NULL;
   // Reset the current item pointer to the start of the list so we can print the
   // list to stdout
   current_item = list;
